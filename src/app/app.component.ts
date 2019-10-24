b4a76799d7c38a094ae8a6cab753becb54f0548e
taitruong <maitai.truong@gmail.com>
2019-10-24 21:36:33 +0200

    first draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { CdkDragDrop, moveItemInArray } from "@angular/cdk/drag-drop";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  boxWidth = 155;
+  items: Array<number> = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  itemTable: Array<number[]> = new Array(this.items.length);;
+
+  getTableRow(articleRowElement: Element, index: number): number[] {
+    const columnSize = this.getColumnSize(articleRowElement, index);
+    const column = index % columnSize;
+    if (column == 0) {
+      const row = this.getRow(index, columnSize);
+      let data = this.itemTable[row];
+      if (!data) {
+
+        data = this.items.slice(row * columnSize, (row + 1) * columnSize);
+        this.itemTable[row] = data;
+      }
+      return data;
+    }
+    return [];
+  }
+
+  updateItemTable() {
+    let index = 0;
+    this.itemTable = this.itemTable.map(row =>
+      row.map(articleFormGroup => this.items[index++])
+    );
+  }
+
+  getColumnSize(articleRowElement: Element, index: number): number {
+    const { width } = articleRowElement.getBoundingClientRect();
+    console.log('>>> total width', width);
+    const columnSize = (width - (width % this.boxWidth)) / this.boxWidth;
+    console.log('>>>> column size', columnSize);
+    return columnSize;
+  }
+
+  getRow(index: number, columnSize: number) {
+    const column = index % columnSize;
+    return (index - column) / columnSize;
+  }
+
+  reorderDroppedList(event: CdkDragDrop<number[]>) {
+    const { previousIndex, currentIndex } = event;
+
+    const previousFormGroup = event.previousContainer.data[previousIndex];
+    const currentFormGroup =
+      currentIndex < event.container.data.length
+        ? event.container.data[currentIndex]
+        : event.container.data[currentIndex - 1];
+
+    const previousSortIndex = this.items.indexOf(previousFormGroup);
+    let currentSortIndex =
+      currentIndex < event.container.data.length
+        ? this.items.indexOf(currentFormGroup)
+        : this.items.indexOf(currentFormGroup) + 1;
+    if (
+      previousSortIndex < currentSortIndex &&
+      event.previousContainer != event.container
+    ) {
+      currentSortIndex--;
+    }
+
+    moveItemInArray(this.items, previousIndex, currentIndex);
+
+    this.updateItemTable();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { DragDropModule } from "@angular/cdk/drag-drop";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, DragDropModule, FlexLayoutModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
