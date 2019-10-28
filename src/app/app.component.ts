f0c8f4ee677a5ef2f434a568bc4ecad22c774ea2
Tai 'Mr. T' Truong <tai.truong@software-developer.org>
2019-10-28 20:29:14 +0100

    fix row size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import {
 export class AppComponent {
   // width 150px + 5px margin
   boxWidth = 155;
-  items: Array<number> = Array.from({ length: 20 }, (v, k) => k + 1);
+  items: Array<number> = Array.from({ length: 21 }, (v, k) => k + 1);
   itemsTable: Array<number[]>;
 
   getItemsTable(rowLayout: Element): number[][] {
@@ -24,7 +24,8 @@ export class AppComponent {
     const { width } = rowLayout.getBoundingClientRect();
     const columnSize = Math.round(width / this.boxWidth);
     // calculate row size: items length / column size
-    const rowSize = Math.round(this.items.length / columnSize);
+    // add 0.5: round up so that last element is shown in next row
+    const rowSize = Math.round(this.items.length / columnSize + .5);
 
     // create table rows
     const copy = [...this.items];
